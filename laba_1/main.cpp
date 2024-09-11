6cce7362d3c3d0e9be2a170a6ffdf18f953e92a4
ivvenger <vengerir@yandex.ru>
2024-09-11 13:03:14 +0300

    Modified main file.

--- laba_1/main.cpp
+++ laba_1/main.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+string removeVowels(const string& s) {
+    string res;
+    for (char lit : s) {
+        if (lit != 'a' && lit != 'e' && lit != 'i' && lit != 'o' && lit != 'u' && lit != 'y' &&
+            lit != 'A' && lit != 'E' && lit != 'I' && lit != 'O' && lit != 'U' && lit != 'Y') {
+                res += lit;
+        }
+    }
+    return res;
+}
+
+
 int main() {
-    cout << "Bye." << endl;
+    string s;
+    cout << "Введите строку: ";
+    getline(cin, s);
+    string output = removeVowels(s);
+    cout << "Результат: " << output << endl;
+    return 0;
 }
